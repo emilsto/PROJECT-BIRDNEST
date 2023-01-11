02313a929db5303803f57b055168dce5a7749871
emil <emil.kantaneva@gmail.com>
2023-01-11 22:20:17 +0200

    edited url for deployment

--- bird-app/src/api/axios.tsx
+++ bird-app/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "http://ec2-13-51-194-246.eu-north-1.compute.amazonaws.com:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
